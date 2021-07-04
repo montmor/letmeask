8e34834c33268bed9b8d5e7b804560d4ca43847c
André Mont Mor <andremontmor@hotmail.com>
2021-07-04 20:04:39 -0300

    criando hook para subtituir duas importações: useContext e AuthContext.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -10,11 +10,13 @@ import { useHistory } from "react-router-dom";
 import { AuthContext } from "../../contexts/Auth/AuthContext";
 import { useContext } from 'react';
 
+import { useAuth } from "../../hooks/useAuth";
+
 
 
 export function Home() {
     const history = useHistory();
-    const { user, signInWithGoogle } = useContext(AuthContext);
+    const { user, signInWithGoogle } = useAuth();
 
     async function handleCreateRoom() {
         if (!user) {
