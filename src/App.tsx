02d6a79d4c7f68ddf278390bb425b3d25a3ab01d
André Mont Mor <andremontmor@hotmail.com>
2021-07-04 18:34:15 -0300

    implementação do useContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { BrowserRouter, Route } from "react-router-dom";
+import { createContext, useState } from "react";
 
 import { Home } from "./pages/Home/Home";
 import { NewRoom } from "./pages/NewRoom/NewRoom";
 
+export const TestContext = createContext({} as any);
+
 function App() {
+  const [value, setValue] = useState("teste");
+  
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/rooms/new" component={NewRoom} />
+      <TestContext.Provider value={{value, setValue}}>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </TestContext.Provider>
     </BrowserRouter>
   );
 }
