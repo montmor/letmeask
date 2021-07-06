cd6f900126324636a30a3e767bf94b2ccea26522
André Mont Mor <andremontmor@hotmail.com>
2021-07-05 22:46:34 -0300

    implementação do Toast in CSS

--- src/components/RoomCode/RoomCode.tsx
+++ src/components/RoomCode/RoomCode.tsx
@@ -1,13 +1,27 @@
 import './RoomCode.scss';
 import copyImg from "../../assets/images/copy.svg";
+import { useState } from "react";
 
-export function RoomCode() {
+type RoomCodeProps = {
+    code: string;
+}
+
+export function RoomCode(props: RoomCodeProps) {
+    function copyRoomCodeToClipboard() {
+        navigator.clipboard.writeText(props.code);
+        
+        let x = document.getElementById("snackbar") as any;
+        x.className = "show";
+        setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+    }
+
     return (
-        <button className="room-code">
+        <button className="room-code" onClick={copyRoomCodeToClipboard}>
             <div>
                 <img src={copyImg} alt="Copy room code" />
             </div>
-            <span>#fsdfsd3432423</span>
+            <span>Sala #{props.code}</span>
+            <div id="snackbar">Copy room code: {props.code}</div>
         </button>
     )
 }
--- src/pages/Room/Room.tsx
+++ src/pages/Room/Room.tsx
@@ -9,7 +9,7 @@ export function Room() {
             <header>
                 <div className="content">
                     <img src={logoImg} alt="Letmeask" />
-                    <RoomCode />
+                    <RoomCode code="-Mdt8gSlmcI2VTKmuEzl" />
                 </div>
             </header>
             <main>
