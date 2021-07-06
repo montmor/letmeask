09939042e4ae441a250b42e450346839ebcfb795
André Mont Mor <andremontmor@hotmail.com>
2021-07-06 14:36:57 -0300

    inserindo as informações do usuário logado na página.

--- src/pages/Room/Room.tsx
+++ src/pages/Room/Room.tsx
@@ -62,7 +62,7 @@ export function Room() {
             <main>
                 <div className="room-title">
                     <h1>Sala React</h1>
-                    <span>4 perguntas</span>
+                    {/* <span>4 perguntas</span> */}
                 </div>
                 <form onSubmit={handleSendQuestion}>
                     <textarea
@@ -72,7 +72,16 @@ export function Room() {
                     />
                     
                     <div className="form-footer">
-                        <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+
+                        {user ?
+                            <div className="user-info">
+                                <img src={user.avatar} alt={user.name} />
+                                <span>{user.name}</span>
+                            </div>
+                            :
+                            <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+                        }
+
                         <Button type="submit" disabled={!user}>Enviar pergunta</Button>
                     </div>
                 </form>
