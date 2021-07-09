cf0fa5fb1684d2676a115b6354aa8dc8251068e3
André Mont Mor <andremontmor@hotmail.com>
2021-07-08 22:09:01 -0300

    implementação das funcionalidades do usuário

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,4 +1,5 @@
 import './style.scss';
+import { ReactNode } from 'react';
 
 type QuestionProps = {
   content: string;
@@ -6,9 +7,10 @@ type QuestionProps = {
     name: string;
     avatar: string;
   };
+  children?: ReactNode;
 };
 
-export function Question({ content, author }: QuestionProps) {
+export function Question({ content, author, children }: QuestionProps) {
   return (
     <div className="question">
       <p>{content}</p>
@@ -17,7 +19,7 @@ export function Question({ content, author }: QuestionProps) {
           <img src={author.avatar} alt={author.name} />
           <span>{author.name}</span>
         </div>
-        <div></div>
+        <div>{children}</div>
       </footer>
     </div>
   );
