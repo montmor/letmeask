3a1665ff086e48e2a0d02ab2eb21f86f0fd6d5ff
André Mont Mor <andremontmor@hotmail.com>
2021-07-09 13:12:19 -0300

    Html e css de destaque e marcar como respondida.

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,5 +1,6 @@
 import './style.scss';
 import { ReactNode } from 'react';
+import cx from 'classnames';
 
 type QuestionProps = {
   content: string;
@@ -8,11 +9,25 @@ type QuestionProps = {
     avatar: string;
   };
   children?: ReactNode;
+  isHighLighted?: boolean;
+  isAnsered?: boolean;
 };
 
-export function Question({ content, author, children }: QuestionProps) {
+export function Question({
+  content,
+  author,
+  children,
+  isAnsered = false,
+  isHighLighted = false,
+}: QuestionProps) {
   return (
-    <div className="question">
+    <div
+      className={cx(
+        'question',
+        { answered: isAnsered },
+        { highLighted: isHighLighted && !isAnsered },
+      )}
+    >
       <p>{content}</p>
       <footer>
         <div className="user-info">
--- src/pages/AdminRoom/index.tsx
+++ src/pages/AdminRoom/index.tsx
@@ -1,6 +1,8 @@
 import './style.scss';
 import logoImg from '../../assets/images/logo.svg';
 import deleteImg from '../../assets/images/delete.svg';
+import checkImg from '../../assets/images/check.svg';
+import answerImg from '../../assets/images/answer.svg';
 import { Button } from '../../components/Button';
 import { RoomCode } from '../../components/RoomCode';
 import { Question } from '../../components/Question';
@@ -41,6 +43,18 @@ export function AdminRoom() {
     handleRedirectHome();
   }
 
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnsered: true,
+    });
+  }
+
+  async function handleHighLightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighLighted: true,
+    });
+  }
+
   return (
     <div id="page-room">
       <header>
@@ -62,7 +76,28 @@ export function AdminRoom() {
         <div className="question-list">
           {questions.map((question) => {
             return (
-              <Question key={question.id} author={question.author} content={question.content}>
+              <Question
+                key={question.id}
+                author={question.author}
+                content={question.content}
+                isAnsered={question.isAnsered}
+                isHighLighted={question.isHighLighted}
+              >
+                {!question.isAnsered && (
+                  <>
+                    <button
+                      type="button"
+                      onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                    >
+                      <img src={checkImg} alt="Marcar pergunta como respondida" />
+                    </button>
+
+                    <button type="button" onClick={() => handleHighLightQuestion(question.id)}>
+                      <img src={answerImg} alt="Dar destaque à pergunta" />
+                    </button>
+                  </>
+                )}
+
                 <button type="button" onClick={() => handleDeleteQuestion(question.id)}>
                   <img src={deleteImg} alt="Remover pergunta" />
                 </button>
